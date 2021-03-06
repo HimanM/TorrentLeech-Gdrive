9da03c1de2ed3e77aba7d8c7cbd3058a07520d92
HimanM <67066047+HimanM@users.noreply.github.com>
2021-03-06 08:07:14 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,20 @@
+from tobrot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1669146878:AAFfvv42dNmQKH0hf3zYHNzQYngJH0aU0hA" #imp
+    APP_ID = 3195790 #imp
+    API_HASH = "e504b553e345349e1504caa2872f3dd9" #imp
+    AUTH_CHANNEL = [-1001249352030, 864635890] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev
+    GLEECH_COMMAND = "gleech@drzxleech_bot"
+    YTDL_COMMAND = 'ytdl@drzxleech_bot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@drzxleech_bot"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "Gdrive bot"
+    LEECH_COMMAND = "leech@drzxleech_bot"
+    CANCEL_COMMAND_G = "cancel@drzxleech_bot"
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.-XX9fgU--","token_type":"Bearer","refresh_token":"1//0gLZEp8-VV2rZyourtCgYIARAAGBASNwF-","expiry":"2020-09-03T14:22:34.599776393Z"}\nteam_drive = """
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config
